587dc066e9e0772af2c1481d82dc57459eb31c95
jyfcrw <jyfcrw@gmail.com>
2016-01-12 15:24:07 +0800

    create basic version

---
+++ config.js
@@ -0,0 +1,77 @@
+var config = {
+    // MQTT server configuration, port, etc.
+    mqtt: {
+        port: 1883
+    },
+
+    // HTTP hooks, the events emited will be
+    // forwarded to specified url address.
+    hook: {
+        // auth_url is a hook for authenticating the connection of clients.
+        // method: POST
+        // params:
+        //     cid,  client username
+        //     mode, [digest], only 'digest' is available for now.
+        //     ts,   timestamp
+        // returns:
+        //     token, a string digested by the following rule.
+        // Authenticating rule is, token = md5(username + ":" + ts + ":" + password)
+
+        //  auth_url: "http://localhost:3000/api/v1/auth",
+
+        // auth_pub_url is a hook for authorization of publishing messages to topic.
+        // method: POST
+        // params:
+        //     cid,   client username
+        //     topic, specified topic pattern
+        //     ts,    timestamp
+        // returns:
+        //     mode:  [basic], only 'basic' for now
+        //     value: a boolean value
+
+        //  auth_pub_url: "http://localhost:3000/api/v1/auth/pub",
+
+        // auth_sub_url is a hook for authorization of subscribing topic.
+        // method: POST
+        // params:
+        //     cid,   client username
+        //     topic, specified topic pattern
+        //     ts,    timestamp
+        // returns:
+        //     value: a boolean value
+
+        //  auth_sub_url: "http://localhost:3000/api/v1/auth/sub",
+
+        // client_url is a hook for client state notification.
+        // method: POST
+        // params:
+        //     cid,   client username
+        //     state, [online|offline]
+        //     ts,    timestamp
+        // returns:
+        //     no returns
+        //  client_url: "http://localhost:3000/api/v1/client",
+
+        // message_url is a hook for message forwarding.
+        // method: POST
+        // params:
+        //     cid,      client username
+        //     topic,    specified topic pattern
+        //     payload,  message content
+        //     ts,       timestamp
+        // returns:
+        //     no returns
+        //  message_url: "http://localhost:3000/api/v1/message"
+    },
+
+    // Redis channel name, messages published in the channel, will be
+    // published to specified MQTT topic.
+    // the format is {topic: "xxx", payload: "xxx"} in json.
+    // ex. PUBLISH "mq:listener" "\{\"topic\":\"test\",\"payload\":\"Hello\"\}"
+    listener: "mq:listener",
+
+    // Redis configuration, host or port, etc.
+    redis: {}
+};
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,269 @@
+var mosca = require('mosca'),
+    redis = require('redis'),
+    request = require('request'),
+    cache = require('memory-cache'),
+    crypto = require('crypto');
+
+var config = require('./config');
+
+var rcListenerChannel = config.redis.listener || "mq:listener",
+    rcSenderChannel = config.redis.listener || "mq:sender";
+
+var mqBackendSettings = {
+    type: 'redis',
+    redis: redis,
+    db: 12,
+    host: config.redis.host || "localhost",
+    port: config.redis.port || 6379,
+    return_buffers: true
+};
+
+var mqSettings = {
+    port: config.mqtt.port || 1883,
+    backend: mqBackendSettings,
+    persistence: {
+        factory: mosca.persistence.Redis
+    }
+};
+
+var mqAuthenticateHandle = function(client, username, password, callback) {
+    if (username.substring(0, 6) === "000000") {
+        callback(null, true);
+        return;
+    }
+
+    if (!config.hook.auth_url) {
+        callback(null, true);
+        return;
+    }
+
+    var cacheKey = function() {
+        return 'authentication:' + username;
+    };
+
+    var authenticateToken = function(ts) {
+        return crypto.createHash('md5')
+            .update(username + ":" + ts + ":" + password.toString())
+            .digest('hex');
+    };
+
+    var signature = cache.get(cacheKey());
+    if (signature) {
+        var authorized = (signature.token === authenticateToken(signature.timestamp));
+        if (authorized) client.user = username;
+        callback(null, authorized);
+        return;
+    }
+
+    signature = { timestamp: Date.now() };
+    request.post({
+        url: config.hook.auth_url,
+        json: true,
+        form: {
+            cid: username,
+            mode: "digest",
+            ts: signature.timestamp
+        }
+    }, function (err, resp, body) {
+        if ( resp && resp.statusCode == 200 ) {
+            signature.token = body.token;
+            cache.put(cacheKey(), signature, 1000 * 60 * 5);
+        }
+        console.log('Client signature updated', err.toString());
+
+        var authorized = (signature.token === authenticateToken(signature.timestamp));
+        if (authorized) client.user = username;
+        callback(null, authorized);
+    });
+};
+
+var mqAuthorizePublishHandle = function(client, topic, payload, callback) {
+    if (client.user && client.user.substring(0, 6) === "000000") {
+        callback(null, true);
+        return;
+    }
+
+    if (!config.hook.auth_pub_url) {
+        callback(null, true);
+        return;
+    }
+
+    var cacheKey = function() {
+        return 'authorization:pub:' + client.user + ":" + topic;
+    };
+
+    var authorizePublish = function(passport) {
+        var authorized = false;
+        if (passport.mode === "basic") {
+            authorized = !!passport.value;
+        }
+        return authorized;
+    };
+
+    var passport = cache.get(cacheKey());
+    if (passport) {
+        callback(null, authorizePublish(passport));
+        return;
+    }
+
+    passport = { timestamp: Date.now() };
+    request.post({
+        url: config.hook.auth_pub_url,
+        json: true,
+        form: {
+            cid: client.user,
+            topic: topic,
+            ts: passport.timestamp
+        }
+    }, function (err, resp, body) {
+        if ( resp && resp.statusCode == 200 ) {
+            passport.mode = body.mode || "basic";
+            passport.value = body.value;
+            cache.put(cacheKey(), passport, 1000 * 60 * 5);
+        }
+
+        console.log('Client pub passport updated', err.toString());
+        callback(null, authorizePublish(passport));
+    });
+};
+
+var mqAuthorizeSubscribeHandle = function(client, topic, callback) {
+    if (client.user && client.user.substring(0, 6) === "000000") {
+        callback(null, true);
+        return;
+    }
+
+    if (!config.hook.auth_sub_url) {
+        callback(null, true);
+        return;
+    }
+
+    var cacheKey = function() {
+        return 'authorization:sub:' + client.user + ":" + topic;
+    };
+
+    var authorizeSubscribe = function(passport) {
+        return !!passport.value;
+    };
+
+    var passport = cache.get(cacheKey());
+    if (passport) {
+        callback(null, authorizeSubscribe(passport));
+        return;
+    }
+
+    passport = { timestamp: Date.now() };
+    request.post({
+        url: config.hook.auth_sub_url,
+        json: true,
+        form: {
+            cid: client.user,
+            topic: topic,
+            ts: passport.timestamp
+        }
+    }, function (err, resp, body) {
+        if ( resp && resp.statusCode == 200 ) {
+            passport.value = body.value;
+            cache.put(cacheKey(), passport, 1000 * 60 * 5);
+        }
+
+        console.log('Client sub passport updated', err.toString());
+        callback(null, authorizeSubscribe(passport));
+    });
+};
+
+var mqClientConnectedHandle = function(client) {
+    console.log('Client connected', client.id);
+
+    if (!config.hook.client_url) return;
+
+    request.post({
+        url: config.hook.client_url,
+        form: {
+            cid: client.user,
+            state: "online",
+            ts: Date.now()
+        }
+    }, function (err, resp, body) {
+        console.log('Client state updated [online]', err.toString());
+    });
+};
+
+var mqClientDisconnectingHandle = function(client) {
+    console.log('Client disconnecting:', client.id);
+};
+
+var mqClientDisconnectedHandle = function(client) {
+    console.log('Client disconnected:', client.id);
+
+    if (!config.hook.client_url) return;
+
+    request.post({
+        url: config.hook.client_url,
+        form: {
+            cid: client.user,
+            state: "offline",
+            ts: Date.now()
+        }
+    }, function (err, resp, body) {
+        console.log('Client state updated [offline]', err.toString());
+    });
+};
+
+var mqPublishedHandle = function(packet, client) {
+    if (!client) return;
+
+    console.log('Published', packet.payload);
+
+    if (!config.hook.message_url) return;
+
+    request.post({
+        url: config.hook.message_url,
+        form: {
+            cid: client.user,
+            topic: packet.topic,
+            payload: packet.payload.toString(),
+            ts: Date.now()
+        }
+    }, function (err, resp, body) {
+        console.log('Client message forwarded', err.toString());
+    });
+};
+
+var mqSetupHandle = function() {
+    server.authenticate = mqAuthenticateHandle;
+    server.authorizePublish = mqAuthorizePublishHandle;
+    server.authorizeSubscribe = mqAuthorizeSubscribeHandle;
+    console.log('MQTT Moscer server is up and running');
+};
+
+var rcSubscribeHanlde = function(channel, count) {
+    console.log('Listener is ready on channel:', channel);
+};
+
+var rcMessageHandle = function (channel, message) {
+    if (channel === rcListenerChannel) {
+        try {
+            var packet = JSON.parse(message);
+            if (packet && packet.topic) {
+                server.ascoltatore.publish(packet.topic, packet.payload, function() {
+                  console.log('Message published to the topic', packet.topic, packet.payload);
+                });
+            }
+        } catch(e) {
+            console.log('Message published error:', e.toString());
+        }
+    }
+};
+
+var server = new mosca.Server(mqSettings);
+server.on('ready', mqSetupHandle);
+server.on('clientConnected', mqClientConnectedHandle);
+server.on('clientDisconnecting', mqClientDisconnectingHandle);
+server.on('clientDisconnected', mqClientDisconnectedHandle);
+server.on('published', mqPublishedHandle);
+
+var redisClient = redis.createClient();
+redisClient.subscribe(rcListenerChannel);
+redisClient.on("subscribe", rcSubscribeHanlde);
+redisClient.on("message", rcMessageHandle);
